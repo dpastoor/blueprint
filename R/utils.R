f4e49e9267782a9be49e9680dd16ba83c3d568ab
Devin Pastoor <devin.pastoor@gmail.com>
2017-06-11 18:08:45 +0200

    use and export pipe

--- R/utils.R
+++ R/utils.R
@@ -3,3 +3,13 @@ dots <- function (...)
 {
   rlang::eval_bare(substitute(alist(...)))
 }
+
+#' Pipe operator
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
