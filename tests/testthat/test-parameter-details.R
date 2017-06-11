fc1b28cc471b194b67d3161d8eebef8d56d996d1
Devin Pastoor <devin.pastoor@gmail.com>
2017-06-11 14:27:06 +0200

    test for error in bounds

--- tests/testthat/test-parameter-details.R
+++ tests/testthat/test-parameter-details.R
@@ -10,4 +10,20 @@ describe("parameter_details()", {
                        )
                   )
   })
+  it("saves bounds", {
+    expect_equal(parameter_details(6, c(3, Inf)),
+                  list(value = 6,
+                       bounds = c(3, Inf),
+                       fixed = FALSE,
+                       covariate_relationships = NULL
+                       )
+                  )
+
+  })
+  it("errors when bounds improperly specified", {
+    expect_error(parameter_details(6, c(7)))
+  })
+  it("errors when bounds not in range", {
+    expect_error(parameter_details(6, c(7, Inf)))
+  })
 })
