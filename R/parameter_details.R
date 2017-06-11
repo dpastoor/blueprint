774a34a179781305afc7bc2ce293977bae24c415
Devin Pastoor <devin.pastoor@gmail.com>
2017-06-11 14:21:41 +0200

    create parameter details

---
+++ R/parameter_details.R
@@ -0,0 +1,29 @@
+#' create details about a parameter
+#' @param .value initial value of a parameter
+#' @param .bounds a vector of lower and upper bounds
+#' @param .fixed whether the parameter should be fixed
+#' @param covariate_relationships covariate relationships to a parameter
+#' @export
+parameter_details <- function(.value,
+                              .bounds = NULL,
+                              .fixed = FALSE,
+                              covariate_relationships = NULL) {
+  output <- list(
+    value = .value,
+    bounds = .bounds,
+    fixed = .fixed,
+    covariate_relationships = covariate_relationships
+  )
+  if (!is.null(.bounds)) {
+    if (
+      length(.bounds) != 2 ||
+      .bounds[1] > .bounds[2] ||
+      !dplyr::between(.value, .bounds[1], .bounds[2])
+    ) {
+      stop("bounds must enclose value, and should be a vector, eg c(0, Inf)")
+    }
+    output$bounds <- .bounds
+
+  }
+  return(output)
+}
