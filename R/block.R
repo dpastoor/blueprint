41f53d5c7b9a0f6756b7b4988a2d885d1ffefb00
Devin Pastoor <devin.pastoor@gmail.com>
2017-07-05 09:19:27 -0400

    export and store whether values should be fixed

--- R/block.R
+++ R/block.R
@@ -7,11 +7,31 @@
 #' block(0.04, 0.01, 0.09, .params = vars(CL, V))
 #' # can also set as correlation matrix
 #' block(0.04, 0.61, 0.09, .params = c("CL", "V"), correlation = TRUE)
+#' @export
 block <- function(..., .params, .fix = FALSE, correlation = FALSE) {
   output_matrix <- mrgsolve::bmat(..., correlation = correlation)
   return(list(type = "block",
               params = .params,
               correlation = correlation,
+              values = output_matrix)
+         )
+}
+
+#' diagonal values
+#' @param ... elements to coerce into a block matrix
+#' @param .fix whether the values should be fixed
+#' @param .params parameters associated with the elements of the matrix
+#' @param correlation whether to create as a correlation matrix
+#' @examples
+#' block(0.04, 0.01, 0.09, .params = vars(CL, V))
+#' # can also set as correlation matrix
+#' block(0.04, 0.61, 0.09, .params = c("CL", "V"), correlation = TRUE)
+block <- function(..., .params, .fix = FALSE, correlation = FALSE) {
+  output_matrix <- mrgsolve::bmat(..., correlation = correlation)
+  return(list(type = "block",
+              params = .params,
+              correlation = correlation,
+              fix = .fix,
               values = output_matrix)
          )
 }
--- tests/testthat/test-block.R
+++ tests/testthat/test-block.R
@@ -3,7 +3,7 @@ context("block")
 describe("block() works", {
   it("can be initialized correctly", {
     input <- block(0.4, 0.6, 0.09, .params = c("CL", "V"), correlation = TRUE)
-    expect_equal(input, list(type = "block", params = c("CL", "V"), correlation = TRUE,
+    expect_equal(input, list(type = "block", params = c("CL", "V"), correlation = TRUE, fix = FALSE,
                   values = structure(c(0.4,
                                        0.113841995766062,
                                        0.113841995766062,
