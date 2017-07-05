16ca958ec820613522357a89444a0bef26c9c10a
Devin Pastoor <devin.pastoor@gmail.com>
2017-07-05 09:16:27 -0400

    require params to match by name for now

--- R/block.R
+++ R/block.R
@@ -2,11 +2,12 @@
 #' @param ... elements to coerce into a block matrix
 #' @param .fix whether the values should be fixed
 #' @param .params parameters associated with the elements of the matrix
+#' @param correlation whether to create as a correlation matrix
 #' @examples
 #' block(0.04, 0.01, 0.09, .params = vars(CL, V))
 #' # can also set as correlation matrix
 #' block(0.04, 0.61, 0.09, .params = c("CL", "V"), correlation = TRUE)
-block <- function(..., .params = NULL, .fix = FALSE, correlation = FALSE) {
+block <- function(..., .params, .fix = FALSE, correlation = FALSE) {
   output_matrix <- mrgsolve::bmat(..., correlation = correlation)
   return(list(type = "block",
               params = .params,
