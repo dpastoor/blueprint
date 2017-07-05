40ad2570d3bf62944e00b1371336fe74f1c3c3da
Devin Pastoor <devin.pastoor@gmail.com>
2017-07-05 16:37:06 -0400

    update tests to reflect block changes

--- tests/testthat/test-block.R
+++ tests/testthat/test-block.R
@@ -3,16 +3,23 @@ context("block")
 describe("block() works", {
   it("can be initialized correctly", {
     input <- block(0.4, 0.6, 0.09, .params = c("CL", "V"), correlation = TRUE)
-    expect_equal(input, list(type = "block",
+    expect_equal(input, list(block = TRUE,
                              params = c("CL", "V"),
                              fix = FALSE,
                              correlation = TRUE,
-                  values = structure(c(0.4,
+                  matrix = structure(c(0.4,
                                        0.113841995766062,
                                        0.113841995766062,
                                        0.09),
                                      .Dim = c(2L,
                                             2L)),
+                  value = stringify_matrix(structure(c(0.4,
+                                                        0.113841995766062,
+                                                        0.113841995766062,
+                                                        0.09),
+                                                      .Dim = c(2L,
+                                                               2L))),
+                  nomega = 2,
                   comment = NULL
                   )
                  )
