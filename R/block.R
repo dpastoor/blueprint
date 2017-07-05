21bf36f0f089db6f350d81b9deec2d93849ed684
Devin Pastoor <devin.pastoor@gmail.com>
2017-07-05 09:15:00 -0400

    add block
    implement block creation for omega or sigma matrices

--- R/block.R
+++ R/block.R
@@ -2,11 +2,15 @@
 #' @param ... elements to coerce into a block matrix
 #' @param .fix whether the values should be fixed
 #' @param .params parameters associated with the elements of the matrix
-#' @example
+#' @examples
 #' block(0.04, 0.01, 0.09, .params = vars(CL, V))
 #' # can also set as correlation matrix
-#' block(0.04, 0.61, 0.09, correlation = TRUE, .params = vars(CL, V))
-block <- function(..., .fix = FALSE, .params) {
-  output_matrix <- mrgsolve::bmat(...)
-  return(output_matrix)
+#' block(0.04, 0.61, 0.09, .params = c("CL", "V"), correlation = TRUE)
+block <- function(..., .params = NULL, .fix = FALSE, correlation = FALSE) {
+  output_matrix <- mrgsolve::bmat(..., correlation = correlation)
+  return(list(type = "block",
+              params = .params,
+              correlation = correlation,
+              values = output_matrix)
+         )
 }
---
+++ tests/testthat/test-block.R
@@ -0,0 +1,16 @@
+context("block")
+
+describe("block() works", {
+  it("can be initialized correctly", {
+    input <- block(0.4, 0.6, 0.09, .params = c("CL", "V"), correlation = TRUE)
+    expect_equal(input, list(type = "block", params = c("CL", "V"), correlation = TRUE,
+                  values = structure(c(0.4,
+                                       0.113841995766062,
+                                       0.113841995766062,
+                                       0.09),
+                                     .Dim = c(2L,
+                                            2L))
+                  )
+                 )
+  })
+})
