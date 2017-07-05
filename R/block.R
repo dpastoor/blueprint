2ebb89dbf1d5724d98d8d55af79bf52bce036b83
Devin Pastoor <devin.pastoor@gmail.com>
2017-07-05 09:41:49 -0400

    omega heirarchy updates
    update block to track comments and fixed status.
    
    add omega_param to capture a single parameter

--- R/block.R
+++ R/block.R
@@ -3,35 +3,34 @@
 #' @param .fix whether the values should be fixed
 #' @param .params parameters associated with the elements of the matrix
 #' @param correlation whether to create as a correlation matrix
+#' @param .comment comment to append
 #' @examples
 #' block(0.04, 0.01, 0.09, .params = vars(CL, V))
 #' # can also set as correlation matrix
 #' block(0.04, 0.61, 0.09, .params = c("CL", "V"), correlation = TRUE)
 #' @export
-block <- function(..., .params, .fix = FALSE, correlation = FALSE) {
+block <- function(..., .params, .fix = FALSE, correlation = FALSE, .comment = NULL) {
   output_matrix <- mrgsolve::bmat(..., correlation = correlation)
   return(list(type = "block",
               params = .params,
+              fix = .fix,
               correlation = correlation,
-              values = output_matrix)
+              values = output_matrix,
+              comment = .comment
+              )
          )
 }
 
 #' diagonal values
-#' @param ... elements to coerce into a block matrix
-#' @param .fix whether the values should be fixed
-#' @param .params parameters associated with the elements of the matrix
-#' @param correlation whether to create as a correlation matrix
-#' @examples
-#' block(0.04, 0.01, 0.09, .params = vars(CL, V))
-#' # can also set as correlation matrix
-#' block(0.04, 0.61, 0.09, .params = c("CL", "V"), correlation = TRUE)
-block <- function(..., .params, .fix = FALSE, correlation = FALSE) {
-  output_matrix <- mrgsolve::bmat(..., correlation = correlation)
-  return(list(type = "block",
-              params = .params,
-              correlation = correlation,
+#' @param .param parameter name
+#' @export
+omega_param <- function(.value, .param, .fix = FALSE, .comment = NULL) {
+  # note omega param takes param and value singular, vs the block
+  return(list(type = "diagonal",
+              param = .param,
               fix = .fix,
-              values = output_matrix)
-         )
+              correlation = FALSE,
+              value = .value,
+              comment = .comment))
+
 }
