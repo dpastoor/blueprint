96de15a66a26c3db5d81108e810aba665aa4a9ad
Devin Pastoor <devin.pastoor@gmail.com>
2017-07-16 13:18:02 -0400

    change parameter to be a class

--- R/parameter_details.R
+++ R/parameter_details.R
@@ -1,46 +1,134 @@
-#' create details about a parameter
-#' @param .value initial value of a parameter
-#' @param .lower_bound a lower bound
-#' @param .upper_bound an upper bound
-#' @param .fixed whether the parameter should be fixed
-#' @param covariate_relationships covariate relationships to a parameter
-#' @param .name name of parameter if can be customized (software dependent)
-#' @export
-param <- function(.value,
-                  .comment = NULL,
-                  .lower_bound = NULL,
-                  .upper_bound = NULL,
-                  .fixed = FALSE,
-                  covariate_relationships = NULL,
-                  .name = NULL) {
-  output <- list(
-    value = .value,
-    comment = .comment,
-    lower_bound = .lower_bound,
-    upper_bound = .upper_bound,
-    fixed = .fixed,
-    covariate_relationships = covariate_relationships,
-    name = .name
-  )
-  if (!is.null(.lower_bound) || !is.null(.upper_bound)) {
-    lb <- ifelse(is.null(.lower_bound), -Inf, .lower_bound)
-    ub <- ifelse(is.null(.upper_bound), Inf, .upper_bound)
-    if (ub < lb) {
-      stop("upper bound must be greater than lower bound")
-    }
-    if (
-      !dplyr::between(.value, lb, ub)
-    ) {
-      stop("bounds must enclose value")
-    }
-  }
-  return(output)
-}
-
 #' create a constant parameter
 #' @param .value value to hold
 #' @param .comment comment
 #' @export
 const <- function(.value, .comment) {
   return(list(value = .value, comment = .comment))
+}
+
+#'
+Parameter <- R6Class("Parameter",
+                  public = list(
+                  initialize =  function(
+                  name,
+                  value,
+                  comment = NULL,
+                  lower_bound = -Inf,
+                  upper_bound = Inf,
+                  fixed = FALSE,
+                  covariate_relationships = NULL,
+                  link = name
+                  ) {
+                  bounds_check(value, lower_bound, upper_bound)
+
+                  private$name <- name
+                  private$value <- value
+                  private$comment <- comment
+                  private$lower_bound <- lower_bound
+                  private$upper_bound <- upper_bound
+                  private$fixed <- fixed
+                  private$covariate_relationships <- covariate_relationships
+                  private$link <- link
+                  },
+
+                  ## getters ------
+                  get_name = function() {
+                    private$name
+                  },
+                  get_value = function() {
+                    private$value
+                  },
+                  get_comment = function() {
+                    private$comment
+                  },
+                  get_lower_bound = function() {
+                    private$lower_bound
+                  },
+                  get_upper_bound = function() {
+                    private$upper_bound
+                  },
+                  get_fixed = function() {
+                    private$fixed
+                  },
+                  get_covariate_relationships = function() {
+                    private$covariate_relationships
+                  },
+                  get_link = function() {
+                    private$link
+                  },
+                  get_all_information = function() {
+                    list(
+                      "name" = private$name,
+                      "value" = private$value,
+                      "comment" = private$comment,
+                      "lower_bound" = private$lower_bound,
+                      "upper_bound" = private$upper_bound,
+                      "fixed" = private$fixed,
+                      "covariate_relationships" = private$covariate_relationships,
+                      "link" = private$link
+                    )
+                  },
+                  ## setters ------
+                  set_name = function(.x, link = FALSE) {
+                    private$name <- .x
+                    if (link) {
+                      private$link <- .x
+                    }
+                  },
+                  set_value = function(.x) {
+                    private$value <- .x
+                  },
+                  set_comment = function(.x) {
+                    private$comment <- .x
+                  },
+                  set_lower_bound = function(.x) {
+                    # so can use NULL to wipe out user settings so will go back to -Inf
+                    if (is.null(.x)) {
+                      .x <- -Inf
+                    }
+                    bounds_check(private$value, .x, private$upper_bound)
+                    private$lower_bound <- .x
+                  },
+                  set_upper_bound = function(.x) {
+                    # so can use NULL to wipe out user settings so will go back to Inf
+                    if (is.null(.x)) {
+                      .x <- Inf
+                    }
+                    bounds_check(private$value, private$lower_bound, private$upper_bound)
+                    private$upper_bound <- .x
+                  },
+                  set_fixed = function(.x) {
+                    if (!rlang::is_bare_logical(.x, 1)) {
+                      stop("fixed must be a boolean value TRUE/FALSE")
+                    }
+                    private$fixed <- .x
+                  },
+                  set_covariate_relationships = function(.x) {
+                    stop("not actually implemented - need to set and update if exists")
+                    private$covariate_relationships <- .x
+                  },
+                  set_link = function(.x) {
+                    private$link <- .x
+                  }
+                  ),
+                 private = list(
+                  name = NULL,
+                  value = NULL,
+                  comment = NULL,
+                  lower_bound = NULL,
+                  upper_bound = NULL,
+                  fixed = NULL,
+                  covariate_relationships = NULL,
+                  link = NULL
+                 )
+)
+
+bounds_check <- function(.value, .lb, .ub) {
+    if (.ub < .lb) {
+      stop("upper bound must be greater than lower bound")
+    }
+    if (!dplyr::between(.value, .lb, .ub)) {
+      stop(glue::glue("bounds must enclose value {.value}"))
+    }
+  return(invisible())
 }
--- tests/testthat/test-parameter-details.R
+++ tests/testthat/test-parameter-details.R
@@ -1,31 +1,28 @@
-context("param")
+context("parameters")
 
-describe("param() works", {
-  it("creates a parameter given a value", {
-    expect_equal(param(6),
-                  list(value = 6,
-                       comment = NULL,
-                       lower_bound = NULL,
-                       upper_bound = NULL,
-                       fixed = FALSE,
-                       covariate_relationships = NULL
-                       )
-                  )
+describe("Parameter works", {
+  it("creates a basic parameter given a value", {
+    param1 <- Parameter$new("tmp", 6)
+    expect_equal(param1$get_value(), 6)
+    expect_equal(param1$get_lower_bound(), -Inf)
+    expect_equal(param1$get_upper_bound(), Inf)
   })
   it("saves bounds", {
-    expect_equal(param(6, .lower_bound = 3),
-                  list(value = 6,
-                       comment = NULL,
-                       lower_bound = 3,
-                       upper_bound = NULL,
-                       fixed = FALSE,
-                       covariate_relationships = NULL
-                       )
-                  )
-
+    param2 <- Parameter$new("tmp", 6, lower_bound = 3)
+    expect_equal(param2$get_value(), 6)
+    expect_equal(param2$get_lower_bound(), 3)
   })
   it("errors when bounds not in range", {
-    expect_error(param(6, .lower_bound = 2, .upper_bound = 4))
-    expect_error(param(6, .lower_bound = 7))
+    expect_error(Parameter$new("tmp", 6, lower_bound = 2, upper_bound = 4))
+    expect_error(Parameter$new("tmp", 6, lower_bound = 7))
+    expect_error(Parameter$new("tmp", 6, upper_bound = 3))
+  })
+  it("can give its private fields as a list for templating", {
+    expect_equal(Parameter$new("tmp", 6, lower_bound = 3)$get_all_information(),
+         structure(list(name = "tmp", value = 6, comment = NULL, lower_bound = 3,
+                        upper_bound = Inf, fixed = FALSE, covariate_relationships = NULL,
+                        link = "tmp"),
+                   .Names = c("name", "value", "comment", "lower_bound",
+                            "upper_bound", "fixed", "covariate_relationships", "link")))
   })
 })
