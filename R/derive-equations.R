6dfadeffdb380e4252b0e1b8b9d9570eb4ca0d01
Devin Pastoor <devin.pastoor@gmail.com>
2017-07-16 22:15:15 -0400

    update nonmem equation derivation to only set tvparam naming if has omega, else if someone wants to be explicit should set the name

--- R/derive-equations.R
+++ R/derive-equations.R
@@ -1,34 +1,44 @@
 equation_derivations = function(type) {
   switch(type,
          mrgsolve = derive_equations_mrgsolve,
-         nonmem = derive_equations
+         nonmem = derive_equations_nonmem
          )
 }
-#' derive equations
-derive_equations <- function(elements) {
-  params <- elements$parameters
+#' derive equations for nonmem
+derive_equations_nonmem <- function(elements) {
+  params <- map(elements$parameters, ~ .x$get_all_information())
   omegas <- elements$omegas
 
-  param_names <- names(params)
-
-  omega_names <- purrr::map2(omegas, names(omegas), function(.omega, .name) {
-    #
-    if (.omega$block) {
-      return(.omega$params)
-    }
-    # for diagonal elements just return the name
-    return(.name)
-  }) %>% purrr::flatten() %>% unlist()
+  param_names <- purrr::map_chr(params, ~ .x$link)
+  if (length(omegas)) {
+    omega_names <- purrr::map2(omegas, names(omegas), function(.omega, .name) {
+      #
+      if (.omega$block) {
+        return(.omega$params)
+      }
+      # for diagonal elements just return the name
+      return(.name)
+    }) %>% purrr::flatten() %>% unlist()
+  } else {
+    omega_names <- c()
+  }
 
   both_names <- intersect(param_names, omega_names)
   no_random_effect <- setdiff(param_names, omega_names)
-  tvs <- purrr::map(param_names, ~ sprintf("TV%s = THETA(%s)", .x, which(param_names == .x)))
-  fulls <- purrr::map(both_names, function(.p) {
-    sprintf("%s = TV%s*EXP(ETA(%s))", .p, .p, which(omega_names == .p))
-  })
+
   nofulls <- purrr::map(no_random_effect, function(.p) {
-    sprintf("%s = TV%s", .p, .p)
+    sprintf("%s = THETA(%s)", .p, which(param_names == .p))
   })
+
+  if (length(both_names)) {
+    tvs <- purrr::map(both_names, ~ sprintf("TV%s = THETA(%s)", .x, which(param_names == .x)))
+    fulls <- purrr::map(both_names, function(.p) {
+      sprintf("%s = TV%s*EXP(ETA(%s))", .p, .p, which(omega_names == .p))
+    })
+  } else {
+    fulls <- list()
+    tvs <- list()
+  }
   # need to flatten one level so not a list of lists
   return(purrr::flatten(list(tvs, fulls, nofulls)))
 }
@@ -38,8 +48,7 @@ derive_equations_mrgsolve <- function(elements) {
   params <- elements$parameters
   omegas <- elements$omegas
 
-  param_names <- names(params)
-
+  param_names <- purrr::map_chr(params, ~ .x$link)
   omega_names <- purrr::map2(omegas, names(omegas), function(.omega, .name) {
     #
     if (.omega$block) {
