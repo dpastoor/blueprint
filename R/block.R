3a1b4a509c8a8be5f78bc0732b21146c718a3796
Devin Pastoor <devin.pastoor@gmail.com>
2017-08-25 11:40:34 -0400

    remote .param notation and generalize block a little
    dot notation unncessary for these longer parameter names and could be confusing. Since block could also be used for a sigma block keeping the nomenclature a little more general

--- R/block.R
+++ R/block.R
@@ -1,53 +1,54 @@
 #' block matrix
 #' @param ... elements to coerce into a block matrix
-#' @param .fix whether the values should be fixed
-#' @param .params parameters associated with the elements of the matrix
+#' @param fix whether the values should be fixed
+#' @param param_names parameters associated with the elements of the matrix
 #' @param correlation whether to create as a correlation matrix
-#' @param .comment comment to append
+#' @param comment comment to append
 #' @examples
 #' block(0.04, 0.01, 0.09, .params = vars(CL, V))
 #' # can also set as correlation matrix
 #' block(0.04, 0.61, 0.09, .params = c("CL", "V"), correlation = TRUE)
 #' @export
-block <- function(..., .params, .fix = FALSE, correlation = FALSE, .comment = NULL) {
+block <- function(..., param_names, fix = FALSE, correlation = FALSE, comment = NULL) {
   output_matrix <- mrgsolve::bmat(..., correlation = correlation)
   return(list(block = TRUE,
-              params = .params,
-              fix = .fix,
+              params = param_names,
+              fix = fix,
               correlation = correlation,
               matrix = output_matrix,
-              value = stringify_matrix(output_matrix, .fix),
-              nomega = length(.params),
-              comment = .comment
+              value = stringify_matrix(output_matrix, fix),
+              num_params = length(param_names),
+              comment = comment
               )
          )
 }
 
+
 #' diagonal values
-#' @param .value value
-#' @param .fix whether fixed DEFAULT: FALSE
-#' @param .comment comment
+#' @param value value
+#' @param fix whether fixed DEFAULT: FALSE
+#' @param comment comment
 #' @export
-omega_param <- function(.value, .fix = FALSE, .comment = NULL) {
+omega_param <- function(value, fix = FALSE, comment = NULL) {
   # note omega param takes param and value singular, vs the block
   return(list(block = FALSE,
-              fix = .fix,
+              fix = fix,
               correlation = FALSE,
-              value = .value,
-              comment = .comment))
+              value = value,
+              comment = comment))
 
 }
 #' diagonal values
-#' @param .value value
-#' @param .fix whether fixed DEFAULT: FALSE
-#' @param .comment comment
+#' @param value value
+#' @param fix whether fixed DEFAULT: FALSE
+#' @param comment comment
 #' @export
-sigma_param <- function(.value, .fix = FALSE, .comment = NULL) {
+sigma_param <- function(value, fix = FALSE, comment = NULL) {
   # note omega param takes param and value singular, vs the block
   return(list(type = "diagonal",
-              fix = .fix,
+              fix = fix,
               correlation = FALSE,
-              value = .value,
-              comment = .comment))
+              value = value,
+              comment = comment))
 
 }
