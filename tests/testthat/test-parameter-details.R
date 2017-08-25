c29b82975cb9b9c0425a034c3289f702f8d386a1
Devin Pastoor <devin.pastoor@gmail.com>
2017-08-25 08:26:36 -0400

    update existing tests to s3 semantics

--- tests/testthat/test-parameter-details.R
+++ tests/testthat/test-parameter-details.R
@@ -2,27 +2,31 @@ context("parameters")
 
 describe("Parameter works", {
   it("creates a basic parameter given a value", {
-    param1 <- Parameter$new(6)
-    expect_equal(param1$get_value(), 6)
-    expect_equal(param1$get_lower_bound(), -Inf)
-    expect_equal(param1$get_upper_bound(), Inf)
+    param1 <- parameter(6)
+    expect_equal(value(param1), 6)
+    expect_equal(lower_bound(param1), -Inf)
+    expect_equal(upper_bound(param1), Inf)
   })
   it("saves bounds", {
-    param2 <- Parameter$new(6, lower_bound = 3)
-    expect_equal(param2$get_value(), 6)
-    expect_equal(param2$get_lower_bound(), 3)
+    param2 <- parameter(6, lower_bound = 3)
+    expect_equal(value(param2), 6)
+    expect_equal(lower_bound(param2), 3)
   })
   it("errors when bounds not in range", {
-    expect_error(Parameter$new(6, lower_bound = 2, upper_bound = 4))
-    expect_error(Parameter$new(6, lower_bound = 7))
-    expect_error(Parameter$new(6, upper_bound = 3))
+    expect_error(parameter(6, lower_bound = 2, upper_bound = 4))
+    expect_error(parameter(6, lower_bound = 7))
+    expect_error(parameter(6, upper_bound = 3))
   })
-  it("can give its private fields as a list for templating", {
-    expect_equal(Parameter$new(6, lower_bound = 3)$get_all_information(),
+  it("constructs all elements", {
+    expect_equal(parameter(6, lower_bound = 3),
          structure(list(name = NULL, value = 6, comment = NULL, lower_bound = 3,
                         upper_bound = Inf, fixed = FALSE, covariate_relationships = NULL,
                         link = NULL),
                    .Names = c("name", "value", "comment", "lower_bound",
-                            "upper_bound", "fixed", "covariate_relationships", "link")))
+                            "upper_bound", "fixed", "covariate_relationships", "link"), class = "parameter"))
+  })
+  it("errors when updating elements that don't exist", {
+    param1 <- parameter(6)
+    expect_error(update(param1, notExist = 2))
   })
 })
