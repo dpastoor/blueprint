87b3853eaf10cdfed29a5b976e72d3be955bbe90
Devin Pastoor <devin.pastoor@gmail.com>
2017-08-30 20:40:56 -0400

    update blocktest for class output

--- tests/testthat/test-block.R
+++ tests/testthat/test-block.R
@@ -3,25 +3,14 @@ context("block")
 describe("block() works", {
   it("can be initialized correctly", {
     input <- block(0.4, 0.6, 0.09, param_names = c("CL", "V"), correlation = TRUE)
-    expect_equal(input, list(block = TRUE,
-                             params = c("CL", "V"),
-                             fix = FALSE,
-                             correlation = TRUE,
-                  matrix = structure(c(0.4,
-                                       0.113841995766062,
-                                       0.113841995766062,
-                                       0.09),
-                                     .Dim = c(2L,
-                                            2L)),
-                  value = stringify_matrix(structure(c(0.4,
-                                                        0.113841995766062,
-                                                        0.113841995766062,
-                                                        0.09),
-                                                      .Dim = c(2L,
-                                                               2L))),
-                  num_params = 2,
-                  comment = NULL
-                  )
-                 )
+    output <- structure(list(block = TRUE, params = c("CL", "V"), fix = FALSE,
+                     correlation = TRUE, matrix = structure(c(0.4, 0.113841995766062,
+                                                              0.113841995766062, 0.09), .Dim = c(2L, 2L)),
+                     value = "\n4.000000e-01 \n1.138420e-01 9.000000e-02",
+                     num_params = 2L, comment = NULL), .Names = c("block", "params",
+                                                                  "fix", "correlation",
+                                                                  "matrix", "value", "num_params", "comment"
+                     ), class = c("omega", "block"))
+    expect_equal(input, output)
   })
 })
