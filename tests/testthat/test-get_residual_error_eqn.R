6b1ce1385af31d4efbb4a48a4c2dbb6e449a16c9
Devin Pastoor <devin.pastoor@gmail.com>
2017-09-11 10:53:39 -0400

    add tests for residual error with new api

--- tests/testthat/test-get_residual_error_eqn.R
+++ tests/testthat/test-get_residual_error_eqn.R
@@ -3,27 +3,27 @@ context("get_residual_error_eqn")
 describe("getting residual error mapping works", {
   it("returns ipred if no residual error structure specified", {
     expect_equal(
-      get_residual_error_eqn(c(), "IPRED"),
+      get_residual_error_eqn(c(), "IPRED", "nonmem"),
       "IPRED"
      )
   })
   it("errors if non-supported elements detected", {
-    expect_error(get_residual_error_eqn("BAD", "IPRED"))
+    expect_error(get_residual_error_eqn("BAD", "IPRED", "nonmem"))
   })
   it("sets up simple error structures", {
     expect_equal(
-      get_residual_error_eqn("ADD", "IPRED"),
-      glue::glue("IPRED + ADD")
+      get_residual_error_eqn("ADD", "IPRED", "nonmem"),
+      glue::glue("IPRED + EPS(1)")
     )
     expect_equal(
-      get_residual_error_eqn("PROP", "IPRED"),
-      glue::glue("IPRED*(1+IPRED*PROP)")
+      get_residual_error_eqn("PROP", "IPRED", "nonmem"),
+      glue::glue("IPRED*(1+IPRED*EPS(1))")
     )
   })
   it("sets up combined error structure", {
     expect_equal(
-      get_residual_error_eqn(c("PROP", "ADD"), "IPRED"),
-      glue::glue("IPRED*(1+IPRED*PROP) + ADD")
+      get_residual_error_eqn(c("PROP", "ADD"), "IPRED", "nonmem"),
+      glue::glue("IPRED*(1+IPRED*EPS(1)) + EPS(2)")
     )
   })
 })
