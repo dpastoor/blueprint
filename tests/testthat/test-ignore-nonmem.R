b5c885f574dce754854e63386bbf9d76fc5bba78
Devin Pastoor <devin.pastoor@gmail.com>
2017-09-20 16:04:31 -0400

    start tests

---
+++ tests/testthat/test-ignore-nonmem.R
@@ -0,0 +1,18 @@
+context("ignore:nonmem")
+
+
+describe("ignore statements are generated for nonmem", {
+  tmpl <- "
+{{#ignore}}
+IGNORE=({{{.}}})
+{{/ignore}}
+"
+  it("can construct a full ignore statement", {
+  bp <- Blueprint$new("nonmem")
+  bp$template <- tmpl
+  bp$add_ignores("BQL.EQN.1")
+  expect_equal(bp$render(), "\nIGNORE=(BQL.EQN.1)\n")
+  bp$add_ignores("OBSNUM.GT.10")
+  expect_equal(bp$render(), "\nIGNORE=(BQL.EQN.1)\nIGNORE=(OBSNUM.GT.10)\n")
+  })
+})
