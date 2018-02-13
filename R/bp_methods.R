2b89337bde20703c9dec3be164fdb5ec9e0729c9
Devin Pastoor <devin.pastoor@gmail.com>
2018-02-13 15:17:08 -0500

    start work on constants

--- R/bp_methods.R
+++ R/bp_methods.R
@@ -7,3 +7,13 @@
 parameters <- function(x, ...) {
   UseMethod("parameters", x)
 }
+
+#' generic setting of parameters for blueprints
+#' @param .bp blueprint object
+#' @param ... parameters to set
+#' @param .overwrite whether to overwrite a param if it already exists
+#' @rdname constants
+#' @export
+constants <- function(x, ...) {
+  UseMethod("constants", x)
+}
