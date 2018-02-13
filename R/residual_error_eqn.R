4cf5ccc2c055530710c8e2fb39cf560dcd3b0b16
Devin Pastoor <devin.pastoor@gmail.com>
2018-02-13 15:16:17 -0500

    use dot notation as type will be a generic function

--- R/residual_error_eqn.R
+++ R/residual_error_eqn.R
@@ -1,7 +1,7 @@
 #' get the residual error equation
 #' @param elements residual error elements
 #' @param ipred the name for the IPRED variable
-#' @param type type of model, nonmem or mrgsolve
+#' @param .type type of model, nonmem or mrgsolve
 #' @details
 #' currently supports ADD and PROP
 #' @examples
@@ -9,13 +9,14 @@
 #' get_residual_error_eqn(c("ADD", "PROP"), "IPRED", "nonmem")
 #' get_residual_error_eqn(c("PROP"), "CP", "mrgsolve")
 #' @export
-get_residual_error_eqn <- function(elements, ipred, type) {
+get_residual_error_eqn <- function(elements, ipred, .type) {
   SUPPORTED_ELEMENTS <- c("ADD", "PROP")
   if (!all(elements %in% SUPPORTED_ELEMENTS)) {
     stop(glue("residual error eqn mapping only supports:
-              {paste0(SUPPORTED_ELEMENTS, collapse = '/')}"))
+              {elements}",
+              elements = paste0(SUPPORTED_ELEMENTS, collapse = '/')))
   }
-  name_mapping <- switch(type,
+  name_mapping <- switch(.type,
     nonmem = c("EPS(1)", "EPS(2)"),
     mrgsolve = elements
   )
@@ -24,11 +25,15 @@ get_residual_error_eqn <- function(elements, ipred, type) {
   }
   if (length(elements) == 1) {
     return(switch(elements,
-      ADD = glue("{ipred} + {name_mapping[1]}"),
-      PROP = glue("{ipred}*(1+{ipred}*{name_mapping[1]})")
+      ADD = glue("{ipred} + {.nm}", .nm = name_mapping[1]),
+      PROP = glue("{ipred}*(1+{ipred}*{.nm})", .nm = name_mapping[1])
     ))
   }
-  prop <- ifelse(type == "nonmem", glue("EPS({which(elements == 'PROP')})"), "PROP")
-  add <- ifelse(type == "nonmem", glue("EPS({which(elements == 'ADD')})"), "ADD")
+  prop <- ifelse(.type == "nonmem", glue("EPS({eps_num})",
+                                        eps_num = which(elements == 'PROP')
+                                        ), "PROP")
+  add <- ifelse(.type == "nonmem", glue("EPS({eps_num})",
+                                       eps_num = which(elements == 'ADD')
+                                       ), "ADD")
   return(glue("{ipred}*(1+{ipred}*{prop}) + {add}"))
 }
